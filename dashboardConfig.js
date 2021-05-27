c504a545febf19153b6695e5e5eaaa9eca306889
Lauri Saarni <lauri.saarni@gmail.com>
2021-05-27 10:58:47 +0700

    fix netlify deploy id

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -8,7 +8,7 @@ export default {
             {
               title: 'www.agc-cm.com',
               apiId: 'acda5123-9dfb-4be7-92da-31e5e18f5353',
-              buildHookId: 'https://api.netlify.com/build_hooks/60a372d12e1adcb988a831f8',
+              buildHookId: '60a372d12e1adcb988a831f8',
               name: 'vigorous-nightingale-293448',
               url: 'https://agc-cm.com',
             }
